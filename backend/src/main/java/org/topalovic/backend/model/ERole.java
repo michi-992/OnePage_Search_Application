4e7df3d313f872ca9cf486534d8ace7ed8aa71e0
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-11-02 11:26:40 +0100

    created user entity

---
+++ backend/src/main/java/org/topalovic/backend/model/ERole.java
@@ -0,0 +1,6 @@
+package org.topalovic.backend.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ backend/src/main/java/org/topalovic/backend/model/UserProfile.java
@@ -0,0 +1,56 @@
+package org.topalovic.backend.model;
+
+import jakarta.persistence.*;
+
+import javax.management.relation.Role;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
+    }
+}
