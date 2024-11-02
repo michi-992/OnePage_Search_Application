e537771c30aad7b8fa71b9838e176e3b9f9d2735
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-11-02 14:29:24 +0100

    created user repo

---
+++ backend/src/main/java/org/topalovic/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.topalovic.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.topalovic.backend.model.UserProfile;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUsername(String username);
+}
