54758fbcf035e7e6ba5abb8ad0372eb2d806c9e7
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-11-02 14:43:13 +0100

    added custom error controller

---
+++ backend/src/main/java/org/topalovic/backend/controller/CustomErrorController.java
@@ -0,0 +1,22 @@
+package org.topalovic.backend.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest httpServletRequest) {
+        Object statusCode = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (statusCode != null && Integer.parseInt(statusCode.toString()) == HttpStatus.FORBIDDEN.value()) {
+            return "403";
+        } else if (statusCode != null && Integer.parseInt(statusCode.toString()) == HttpStatus.NOT_FOUND.value()) {
+            return "404";
+        }
+        return "error";
+    }
+}
