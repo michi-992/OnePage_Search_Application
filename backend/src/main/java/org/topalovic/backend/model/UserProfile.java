3ed8a16ab840ba4c6136ccf7c846c2c2cd14d95f
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-11-04 00:34:59 +0100

    deleted wrong import in user profile entity

--- backend/src/main/java/org/topalovic/backend/model/UserProfile.java
+++ backend/src/main/java/org/topalovic/backend/model/UserProfile.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-import javax.management.relation.Role;
 import java.util.HashSet;
 import java.util.Set;
 
