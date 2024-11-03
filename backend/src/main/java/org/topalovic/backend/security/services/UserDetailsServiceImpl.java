cc31867137e87062ebcfecb892a8cd43fe7e7dcc
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-11-04 00:44:34 +0100

    modified/moved user details service implementation

---
+++ backend/src/main/java/org/topalovic/backend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package org.topalovic.backend.security.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.topalovic.backend.model.UserProfile;
+import org.topalovic.backend.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserProfile user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
